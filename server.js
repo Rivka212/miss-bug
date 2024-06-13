0329bbdb39770d8f0a505177e058baab90f0eb3f
Bashari Rivka <basrivka1@gmail.com>
2024-06-14 00:14:46 +0300

    Add functions to bug service

--- server.js
+++ server.js
@@ -1,8 +1,45 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 
+import { bugService } from './services/bug.service.js'
+
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello there'))
-app.listen(3030, () => console.log('Server ready at port 3030'))
+app.use(express.static('public'))
+app.use(cookieParser())
+
+
+app.get('/api/bug/save', (req, res) => {
+    bugService.query()
+        .then(bugs => res.send(bugs))
+    // .catch(err => {
+    //     loggerService.error(`Couldn't get cars...`)
+    //     res.status(500).send(`Couldn't get cars...`)
+    // })
+})
+
+
+app.get('/api/bug/save', (req, res) => {
+    const { _id, title, description, severity, createdAt } = req.query
+    const bugToSave = { _id, title, description, severity: +severity, createdAt: +createdAt }
+
+    bugService.save(bugToSave)
+        .then(savedBug => res.send(savedBug))
+})
+
+app.get('/api/bug/:id', (req, res) => {
+    const { id } = req.params
+    bugService.getById(id)
+        .then(bug => res.send(bug))
+})
+
+app.get('/api/bug/:id/remove', (req, res) => {
+    const { id } = req.params
+    bugService.remove(id)
+        .then(() => res.send(`Bug ${id} deleted...`))
+})
+
+
+app.get('/api/bug', (req, res) => res.send(bugs))
+app.listen(3030, () => console.log('Server ready at 3030'))
---
+++ services/bug.service.js
@@ -0,0 +1,76 @@
+import { utilService } from "./util.service.js"
+
+// const BASE_URL = '/api/bug'
+
+export const bugService = {
+    query,
+    getById,
+    remove,
+    save,
+    getEmptyBug,
+    getDefaultFilter,
+}
+
+var bugs = utilService.readJsonFile('./data/bug.json')
+
+function query() {
+   return Promise.resolve(bags)
+}
+
+
+function getById(bugId) {
+    const bug = bugs.find(bug => bug._id === bugId)
+    return Promise.resolve(bug)
+}
+
+function remove(bugId) {
+    const idx = bugs.findIndex(bug => bug._id === bugId)
+    bugs.splice(idx, 1)
+
+    return _saveBugsToFile()
+}
+
+function save(bugToSave) {
+    if (bugToSave._id) {
+        const idx = bugs.findIndex(bug => bug._id === bugToSave._id)
+        bugs.splice(idx, 1, bugToSave)
+    } else {
+        bugToSave._id = utilService.makeId()
+        bugs.push(bugToSave)
+    }
+    return _saveBugsToFile()
+        .then(() => bugToSave)
+}
+
+function _saveBugsToFile() {
+    return utilService.writeJsonFile('./data/bug.json', bugs)
+}
+
+
+
+// function get(bugId) {
+//     return axios.get(BASE_URL + '/' + bugId)
+//         .then(res => res.data)
+// }
+
+// function remove(bugId) {
+//     return axios.get(BASE_URL + '/' + bugId + '/remove')
+//         .then(res => res.data)
+// }
+
+// function save(bug) {
+//     const queryStr = `/save?title=${bug.title}&description=${bug.description}severity=${bug.severity}
+//     &createdAt=${bug.createdAt}&_id=${bug._id || ''}`
+//     return axios.get(BASE_URL + queryStr)
+//         .then(res => res.data)
+// }
+
+// function getEmptyBug(title = '', description = '', severity = '', createdAt = '') {
+//     return { title, description, severity, createdAt }
+//     // return { vendor, speed }
+// }
+
+// function getDefaultFilter() {
+//     return { title: '', description: '', severity: '', createdAt: '' }
+//     // return { txt: '':'', minSpeed: '' }
+// }

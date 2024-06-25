215eef6ba269e4c863ee15b7f5e52e3b3711de0f
Bashari Rivka <basrivka1@gmail.com>
2024-06-26 00:37:06 +0300

    Add a user route login

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import PDFDocument from 'pdfkit'
@@ -5,17 +6,20 @@ import fs from 'fs'
 
 import { bugService } from './services/bug.service.js'
 import { loggerService } from './services/logger.service.js'
+import { userService } from './services/user.service.js'
 import { log } from 'console'
 
 const app = express()
 
+// Express Config:
 app.use(express.static('public'))
 app.use(cookieParser())
 app.use(express.json())
 
 
+// REST API for Bugs
+// Bug LIST
 app.get('/api/bug', (req, res) => {
-
     const filterBy = {
         txt: req.query.txt || '',
         minSeverity: +req.query.minSeverity || 0,
@@ -24,7 +28,7 @@ app.get('/api/bug', (req, res) => {
         sortDir: +req.query.sortDir || 1,
         labels: req.query.labels || []
     }
-console.log('service filterBy',filterBy);
+
     bugService.query(filterBy)
         .then(bugs => res.send(bugs))
         .catch(err => {
@@ -50,6 +54,7 @@ app.get('/api/bug/download', (req, res) => {
         })
 })
 
+// Bug UPDATE
 app.put('/api/bug/:id', (req, res) => {
     const { _id, title, description, severity, createdAt, labels } = req.body
     const bugToSave = {
@@ -86,7 +91,7 @@ app.get('/api/bug/pageCount', (req, res) => {
         })
 })
 
-
+// Bug CREATE
 app.post('/api/bug/', (req, res) => {
     const { title, description, severity, labels } = req.body
     const bugToSave = { 
@@ -104,6 +109,7 @@ app.post('/api/bug/', (req, res) => {
         })
 })
 
+// Bug READ
 app.get('/api/bug/:id', (req, res) => {
     const { id } = req.params
     var visitedBugs = req.cookies.visitedBugs || []
@@ -116,12 +122,56 @@ app.get('/api/bug/:id', (req, res) => {
         .then(bug => res.send(bug))
 })
 
-
+// Bug DELETE
 app.delete('/api/bug/:id', (req, res) => {
     const { id } = req.params
     bugService.remove(id)
         .then(() => res.send(`Bug ${id} deleted...`))
 })
+
+// AUTH API
+//Users LIST
+app.get('/api/user', (req, res) => {
+    userService.query()
+        .then((users) => {
+            res.send(users)
+        })
+        .catch((err) => {
+            console.log('Cannot load users', err)
+            res.status(400).send('Cannot load users')
+        })
+})
+
+//User READ
+app.get('/api/user/:userId', (req, res) => {
+    const {userId} = req.params
+    userService.getById(userId)
+        .then((user) => {
+            res.send(user)
+        })
+        .catch((err) => {
+            console.log('Cannot load user', err)
+            res.status(400).send('Cannot load user')
+        })
+})
+
+//User LOGIN
+app.post('/api/auth/login', (req, res) => {
+    const credentials = req.body
+    userService.checkLogin(credentials)
+        .then(user => {
+            if (user) {
+                const loginToken = userService.getLoginToken(user)
+                res.cookie('loginToken', loginToken)
+                res.send(user)
+            } else {
+                res.status(401).send('Invalid Credentials')
+            }
+        })
+})
+
+
+
 
 app.get('/**',(req,res)=>{
     res.sendFile(path.resolve('publice/index.html'))
